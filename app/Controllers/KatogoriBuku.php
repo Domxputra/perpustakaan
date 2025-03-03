c5bf4e4fe14eaada31f612364bf9c71a88016dc6
Domxputra <rajinberfoto@mail.com>
2025-03-03 13:13:55 +0900

    new

---
+++ app/Controllers/KatogoriBuku.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class KatogoriBuku extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
